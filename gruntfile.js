912eb87b29f94a1a2aec06b780002f032a0591ce
codebyjonatas <jonatasousa085@gmail.com>
2024-11-15 16:23:12 -0300

    Corrige caminho do script JS na tarefa de substituição para produção
    - Atualiza o caminho do script JavaScript na tarefa 'replace:dist'
    - Altera de '../src/scripts/main.js' para './scripts/main.min.js'
    - Garante que o arquivo HTML final na pasta 'dist' referencie corretamente o script minificado

--- gruntfile.js
+++ gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function (grunt) {
             },
             {
               match: "ENDERECO_JS",
-              replacement: "./src/scripts/main.min.js",
+              replacement: "./scripts/main.min.js",
             },
           ],
         },
